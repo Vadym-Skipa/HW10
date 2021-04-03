1291554f3247ab009ad521d666fcee7b9b3ead61
Vadym-Skipa <lisove2012@gmailcom>
2021-04-03 16:50:54 +0300

    Adding tested classes to tests file

--- test_login_register.py
+++ test_login_register.py
@@ -1,3 +1,126 @@
+# import re
+# import random
+#
+#
+# class RegisterError(Exception):
+#     pass
+#
+#
+# class WrongEmailError(RegisterError):
+#     pass
+#
+#
+# class WrongNameError(RegisterError):
+#     pass
+#
+#
+# class WrongPasswordError(RegisterError):
+#     pass
+#
+#
+# class EmailIsRegistered(RegisterError):
+#     pass
+#
+#
+# class NameIsRegistered(RegisterError):
+#     pass
+#
+#
+# class LogInError(Exception):
+#     pass
+#
+#
+# class EmailIsNotRegistered(LogInError):
+#     pass
+#
+#
+# class WrongLogInPassword(LogInError):
+#     pass
+#
+#
+# class Users:
+#     def __init__(self):
+#         self.users = {}
+#         self.names = []
+#
+#
+# class Register:
+#
+#     @classmethod
+#     def check_email(cls, email: str):
+#         pattern = "[a-zA-Z0-9.]+@[a-z.]+\.[a-z]+"
+#         empty_str, number = re.subn(pattern, "", email)
+#         if empty_str == "" and number == 1:
+#             return True
+#         raise WrongEmailError
+#
+#     @classmethod
+#     def check_name(cls, name: str):
+#         pattern = "\w{6,50}"
+#         empty_str, number = re.subn(pattern, "", name)
+#         if empty_str == "" and number == 1:
+#             return True
+#         raise WrongNameError
+#
+#     @classmethod
+#     def check_password(cls, password: str):
+#         pattern_high_case = "[A-Z]"
+#         pattern_low_case = "[a-z]"
+#         pattern_digit = "\d"
+#         first_str, number_of_high_case = re.subn(pattern_high_case, "", password)
+#         second_str, number_of_low_case = re.subn(pattern_low_case, "", first_str)
+#         empty_str, number_of_digit = re.subn(pattern_digit, "", second_str)
+#         if empty_str == "" and number_of_high_case > 0 and number_of_low_case > 0 and number_of_digit > 0 \
+#                 and 10 <= (number_of_digit + number_of_low_case + number_of_high_case) <= 20:
+#             return True
+#         raise WrongPasswordError
+#
+#     @classmethod
+#     def register(cls, email, name, password, users: Users):
+#         if cls.check_email(email) and cls.check_name(name) and cls.check_password(password):
+#             if email in users.users:
+#                 raise EmailIsRegistered
+#             if name in users.names:
+#                 raise NameIsRegistered
+#             users.users.update({email: (password, name)})
+#             users.names.append(name)
+#             return "200"
+#         raise RegisterError
+#
+#
+# class LogIn:
+#
+#     @classmethod
+#     def check_email(cls, email: str, users: Users):
+#         if Register.check_email(email):
+#             if email in users.users:
+#                 return True
+#         raise EmailIsNotRegistered
+#
+#     @classmethod
+#     def check_password(cls, email: str, password: str, users: Users):
+#         if Register.check_password(password):
+#             if users.users[email][0] == password:
+#                 return True
+#         raise WrongLogInPassword
+#
+#     @classmethod
+#     def log_in(cls, email: str, password: str, users: Users):
+#         if cls.check_email(email, users) and cls.check_password(email, password, users):
+#             return UserToken()
+#         raise LogInError
+#
+#
+# class UserToken:
+#     def __init__(self):
+#         temp_str = ""
+#         number = random.randrange(50, 100)
+#         for i in range(number):
+#             temp_str += random.choice("qwertyuioplkjhgfdsazxcvbnm1234567890QWERTYUIOPLKJHGFDSAZXCVBNM")
+#         self.token = temp_str
+
+
+
 import unittest
 import login_register
 import random
--- test_vacuum_cleaner.py
+++ test_vacuum_cleaner.py
@@ -1,3 +1,199 @@
+# from time import sleep
+#
+# class VacuumCleanerInitError(Exception):
+#     pass
+#
+# class VacuumCleanerError(Exception):
+#     pass
+#
+# class NoWaterError(VacuumCleanerError):
+#     pass
+#
+# class NoBatteryChargeError(VacuumCleanerError):
+#     pass
+#
+# class NoPlaceForGarbage(VacuumCleanerError):
+#     pass
+#
+# class IncorrectBatteryCharge(VacuumCleanerInitError):
+#     pass
+#
+# class IncorrectAmountOfWater(VacuumCleanerInitError):
+#     pass
+#
+# class IncorrectGarbageBinOccupancy(VacuumCleanerInitError):
+#     pass
+#
+# class LowBatteryCharge(Exception):
+#     pass
+#
+#
+# class VacuumCleaner:
+#
+#     config = {"waste_difference": 17, "water_difference": 0.3, "battery_difference_for_washing": 5,
+#               "battery_difference_for_cleaning": 3, "actions_with_low_battery": 6, "max_battery_charge": 100,
+#               "max_amount_of_water": 100, "max_garbage_bin_occupancy": 100, "low_battery": 20}
+#
+#     def __init__(self, battery_charge, amount_of_water, garbage_bin_occupancy):
+#         try:
+#             with open("vacuum_cleaner.conf") as config:
+#                 for line in config:
+#                     key = line.split()[0]
+#                     value = float(line.split()[-1])
+#                     self.config[key] = value
+#         except FileNotFoundError:
+#             print("Config is not found, using default values")
+#         self.waste_difference = self.config["waste_difference"]
+#         self.water_difference = self.config["water_difference"]
+#         self.battery_difference_for_washing = self.config["battery_difference_for_washing"]
+#         self.battery_difference_for_cleaning = self.config["battery_difference_for_cleaning"]
+#         self.actions_with_low_battery = self.config["actions_with_low_battery"]
+#         self.max_battery_charge = self.config["max_battery_charge"]
+#         self.max_amount_of_water = self.config["max_amount_of_water"]
+#         self.max_garbage_bin_occupancy = self.config["max_garbage_bin_occupancy"]
+#         self.low_battery = self.config["low_battery"]
+#         try:
+#             self.amount_of_water = amount_of_water
+#             self.garbage_bin_occupancy = garbage_bin_occupancy
+#             self.battery_charge = battery_charge
+#         except VacuumCleanerInitError as error:
+#             print("Incorrect input params")
+#             raise error
+#         except LowBatteryCharge:
+#             pass
+#
+#     @property
+#     def battery_charge(self):
+#         return self._battery_charge
+#
+#     @battery_charge.setter
+#     def battery_charge(self, battery_charge):
+#         if 0 <= battery_charge <= self.max_battery_charge:
+#             self._battery_charge = battery_charge
+#         else:
+#             raise IncorrectBatteryCharge
+#
+#     @property
+#     def amount_of_water(self):
+#         return self._amount_of_water
+#
+#     @amount_of_water.setter
+#     def amount_of_water(self, amount_of_water):
+#         if 0 <= amount_of_water <= self.max_amount_of_water:
+#             self._amount_of_water = amount_of_water
+#         else:
+#             raise IncorrectAmountOfWater
+#
+#     @property
+#     def garbage_bin_occupancy(self):
+#         return self._garbage_bin_occupancy
+#
+#     @garbage_bin_occupancy.setter
+#     def garbage_bin_occupancy(self, garbage_bin_occupancy):
+#         if 0 <= garbage_bin_occupancy <= self.max_garbage_bin_occupancy:
+#             self._garbage_bin_occupancy = garbage_bin_occupancy
+#         else:
+#             raise IncorrectGarbageBinOccupancy
+#
+#     def water_using_in_washing(self):
+#         try:
+#             self.amount_of_water -= self.water_difference
+#         except IncorrectAmountOfWater:
+#             self.amount_of_water = 0
+#             raise NoWaterError("Washing is not finished")
+#         if self.amount_of_water == 0:
+#             raise NoWaterError("Washing is finished")
+#
+#     def battery_using_in_washing(self):
+#         try:
+#             self.battery_charge -= self.battery_difference_for_washing
+#         except IncorrectBatteryCharge:
+#             self.battery_charge = 0
+#             raise NoBatteryChargeError("Washing is not finished")
+#         if self.battery_charge == 0:
+#             raise NoBatteryChargeError("Washing is finished")
+#         elif self.battery_charge <= self.low_battery:
+#             raise LowBatteryCharge
+#
+#     def wash(self):
+#         try:
+#             self.battery_using_in_washing()
+#         except LowBatteryCharge as error:
+#             self.water_using_in_washing()
+#             print("Wash")
+#             raise error
+#         self.water_using_in_washing()
+#         print("Wash")
+#
+#     def garbage_adding_in_cleaning(self):
+#         try:
+#             self.garbage_bin_occupancy += self.waste_difference
+#         except IncorrectGarbageBinOccupancy:
+#             self.garbage_bin_occupancy = self.max_garbage_bin_occupancy
+#             raise NoPlaceForGarbage("Cleaning is not finished")
+#         if self.garbage_bin_occupancy == self.max_garbage_bin_occupancy:
+#             raise NoPlaceForGarbage("Cleaning is finished")
+#
+#     def battery_using_in_cleaning(self):
+#         try:
+#             self.battery_charge -= self.battery_difference_for_cleaning
+#         except IncorrectBatteryCharge:
+#             self.battery_charge = 0
+#             raise NoBatteryChargeError("Cleaning is not finished")
+#         if self.battery_charge == 0:
+#             raise NoBatteryChargeError("Cleaning is finished")
+#         elif self.battery_charge <= self.low_battery:
+#             raise LowBatteryCharge
+#
+#     def vacuum_cleaner(self):
+#         try:
+#             self.battery_using_in_cleaning()
+#         except LowBatteryCharge as error:
+#             self.garbage_adding_in_cleaning()
+#             print("Vacuum cleaner")
+#             raise error
+#         self.garbage_adding_in_cleaning()
+#         print("Vacuum cleaner")
+#
+#     def move(self):
+#         cleaning_enable = True
+#         washing_enable = True
+#         local_actions_with_low_battery = self.actions_with_low_battery
+#         while local_actions_with_low_battery > 0 and (cleaning_enable or washing_enable):
+#             print(f"battery {self.battery_charge:.1f}, water {self.amount_of_water:.1f} trash, "
+#                   f"{self.garbage_bin_occupancy:.1f}")
+#             print("Move ---")
+#             if washing_enable:
+#                 try:
+#                     self.wash()
+#                 except LowBatteryCharge:
+#                     local_actions_with_low_battery -= 1
+#                 except NoWaterError as error:
+#                     if error.args:
+#                         print(error.args[0])
+#                     print("Water is over")
+#                     washing_enable = False
+#                 except NoBatteryChargeError as error:
+#                     print(error.args[0])
+#                     print("Battery charge is over")
+#                     break
+#             if cleaning_enable:
+#                 try:
+#                     self.vacuum_cleaner()
+#                 except LowBatteryCharge:
+#                     local_actions_with_low_battery -= 1
+#                 except NoPlaceForGarbage as error:
+#                     if error.args:
+#                         print(error.args[0])
+#                     print("Garbage bin is full")
+#                     cleaning_enable = False
+#                 except NoBatteryChargeError as error:
+#                     print(error.args[0])
+#                     print("Battery charge is over")
+#                     break
+#             sleep(1)
+
+
 import unittest
 import vacuum_cleaner
 
